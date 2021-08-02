76a8e06c0a9fecd691e031575387e7e158db764e
Jerbeson Vinycius <jerbesonvinycius@gmail.com>
2021-08-02 20:11:33 -0300

    Ajuste na autenticaçao do usuario

--- src/controllers/authcontroller.py
+++ src/controllers/authcontroller.py
@@ -2,14 +2,21 @@ from src.database.database import sessionLocal
 from sqlalchemy.sql import select
 from src.database.schemas import *
 
+import hashlib
+
 class UserProfileController:
 
   def __init__(self):
     self.session = sessionLocal()
 
   def auth_login(self, data):
+    hash = hashlib.sha512()
+    hash.update(data['password'].encode('utf-8'))
+
+    hashed_password = hash.hexdigest()
+
     query = select(User.id, User.name, User.nickname, User.email, User.password) \
-    .filter(User.email == data['email'], User.password == data['password'])
+    .filter(User.email == data['email'], User.password == hashed_password)
 
     result = self.session.execute(query).fetchone()
     self.session.close()
